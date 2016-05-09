4ac26ee202fd2c29dfcac37ebaca5d3bde80e0b3
David McKay <david@rawkode.com>
2016-05-09 09:39:54 +0100

    Allow a single font-family to be installed, instead of everything by default.

--- install.sh
+++ install.sh
@@ -3,6 +3,21 @@
 # Set source and target directories
 powerline_fonts_dir="${PWD}/patched-fonts"
 
+# Accept a font / directory name, to avoid installing all fonts
+if [ ! -z "$1" ];
+then
+  # Ensure thar directory exists, and offer suggestions if not
+
+  if [[ ! -d "$powerline_fonts_dir/$1" ]];
+  then
+    echo -e "That font directory doesn't exist. Options are: \n"
+    ls "$powerline_fonts_dir"
+    exit -1
+  fi
+
+  powerline_fonts_dir="$powerline_fonts_dir/$1"
+fi
+
 find_command="find \"$powerline_fonts_dir\" \( -name '*.[o,t]tf' -or -name '*.pcf.gz' \) -type f -print0"
 
 if [[ $(uname) == 'Darwin' ]]; then
@@ -22,4 +37,9 @@ if [[ -n $(which fc-cache) ]]; then
   fc-cache -f "$font_dir"
 fi
 
-echo "All fonts installed to $font_dir"
+if [ ! -z "$1" ];
+then
+  echo "$1 font installed to $font_dir"
+else
+  echo "All fonts installed to $font_dir"
+fi
