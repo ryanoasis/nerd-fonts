71561b21cb578bc40d0635405f5d5368fcdb0085
Ryan L McIntyre <ryanoasis@gmail.com>
2016-12-14 20:10:42 -0500

    Completes post processing scripts for Hack (Knack) (fixes #70)

--- bin/scripts/Hack/postprocess.sh
+++ bin/scripts/Hack/postprocess.sh
@@ -4,10 +4,10 @@
 fontfile=$1
 dir=$(dirname "$0")
 
-echo "dir $dir"
+#echo "dir $dir"
 
 "${dir}/autohint.sh" "$fontfile"
-#"${dir}/../fpfix.py" "$fontfile"
+"${dir}/../fpfix.py" "$fontfile"
 
 printf "\n"
 printf "[Nerd Fonts] Post Processed Hack '%s'\n" "$fontfile"
