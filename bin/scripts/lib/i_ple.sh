46b6ff707de62f93052d2f24eb282f5d8670403e
Alex Efros <powerman@powerman.name>
2017-03-31 06:20:08 +0300

    Add shell scripts to map names to icons

---
+++ bin/scripts/lib/i_ple.sh
@@ -0,0 +1,43 @@
+# Powerline Extra Symbols (37 icons, 2 aliases, 16 does not have an established name)
+# Codepoints: E0A0-E0A3 E0B0-E0BF E0C0-E0C8 E0CC-E0CF E0D0-E0D2 E0D4
+test -n "$__i_ple_loaded" && return || __i_ple_loaded=1
+i='' i_pl_branch=$i
+i='' i_pl_line_number=$i
+      i_pl_current_line=$i_pl_line_number
+i='' i_pl_hostname=$i
+      i_pl_readonly=$i_pl_hostname
+i='' i_ple_column_number=$i
+i='' i_pl_left_hard_divider=$i
+i='' i_pl_left_soft_divider=$i
+i='' i_pl_right_hard_divider=$i
+i='' i_pl_right_soft_divider=$i
+i='' i_ple_right_half_circle_thick=$i
+i='' i_ple_right_half_circle_thin=$i
+i='' i_ple_left_half_circle_thick=$i
+i='' i_ple_left_half_circle_thin=$i
+i='' i_ple_lower_left_triangle=$i
+i=''
+i=''
+i=''
+i=''
+i=''
+i=''
+i=''
+i='' i_ple_flame_thick=$i
+i='' i_ple_flame_thin=$i
+i=''
+i=''
+i=''
+i=''
+i=''
+i=''
+i=''
+i='' i_ple_honeycomb=$i
+i='' i_ple_honeycomb_outline=$i
+i='' i_ple_lego_separator=$i
+i='' i_ple_lego_separator_thin=$i
+i='' i_ple_lego_block_facing=$i
+i='' i_ple_lego_block_sideways=$i
+i=''
+i=''
+unset i
