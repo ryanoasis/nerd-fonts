b7497e3e65f1a0957f60c8ed2fe905594826a382
Ryan L McIntyre <ryanoasis@gmail.com>
2018-02-15 19:40:16 -0500

    Updates generated CSS in preparation for Weather Icons (WIP issue #226)

--- bin/scripts/lib/i_all.sh
+++ bin/scripts/lib/i_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script Version 1.1.0
 
-for i in $(dirname "${BASH_SOURCE[0]:-$0}")/i_{dev,fae,fa,iec,linux,oct,ple,pom,seti,material}.sh; do
+for i in $(dirname "${BASH_SOURCE[0]:-$0}")/i_{dev,fae,fa,iec,linux,oct,ple,pom,seti,material,weather}.sh; do
 	# shellcheck source=/dev/null
 	test -f "$i" -a -r "$i" && source "$i"
 done
