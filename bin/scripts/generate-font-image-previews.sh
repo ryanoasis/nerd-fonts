c2041db74f7689cb33160470c2b53cd8d6376a76
Ryan L McIntyre <ryanoasis@users.noreply.github.com>
2019-09-12 14:22:39 -0700

    Updates script to use json data file instead of fc-list

--- bin/scripts/generate-font-image-previews.sh
+++ bin/scripts/generate-font-image-previews.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-output_dir="/home/ryan/projects/nerd-fonts-gh-pages/assets/img/previews/"
-template_svg="lib/inkscape-template-font-preview.svg"
-match_pattern="Nerd Font$"
-readarray -t fonts < <( { fc-list --format="%{family[1]}\n" & fc-list --format="%{family[0]}\n"; } | sort | uniq | grep -i "$match_pattern" )
+main() {
 
-mkdir -p "$output_dir"
+  output_dir="/home/ryan/projects/nerd-fonts-gh-pages/assets/img/previews/"
+  template_svg="lib/inkscape-template-font-preview.svg"
 
-for font in "${fonts[@]}"; do
+  mkdir -p "$output_dir"
+
+  for i in $(jq '.fonts | keys | .[]' lib/fonts.json); do
+    patchedName=$(jq -r ".fonts[$i].patchedName" lib/fonts.json);
+    #unpatchedName=$(jq -r ".fonts[$i].unpatchedName" lib/fonts.json);
+    imagePreviewFont=$(jq -r ".fonts[$i].imagePreviewFont" lib/fonts.json);
+
+    generate_preview "$imagePreviewFont" "$patchedName Nerd Font"
+  done
+
+}
+
+generate_preview() {
+  font=$1
+  fontText=$2
   echo "generating image preview for font: '$font'"
-  sed -e "s/000000/ffffff/" -e "s/sans-serif/${font}/" -e "s/Font Name/${font}/" <"$template_svg" >"${output_dir}${font}.svg"
-  inkscape -f "${output_dir}${font}.svg" --with-gui --verb EditSelectAll --verb ObjectToPath --verb FileVacuum --verb FitCanvasToDrawing --verb FileSave --verb FileQuit
-  svgo "${output_dir}${font}.svg"
-done
+  sed -e "s/000000/ffffff/" -e "s/sans-serif/${font}/" -e "s/Font Name/${fontText}/" <"$template_svg" >"${output_dir}${fontText}.svg"
+  inkscape -f "${output_dir}${fontText}.svg" --with-gui --verb EditSelectAll --verb ObjectToPath --verb FileVacuum --verb FitCanvasToDrawing --verb FileSave --verb FileQuit &>/dev/null &
+  svgo "${output_dir}${fontText}.svg"
+}
+
+main "$@"; exit
 
