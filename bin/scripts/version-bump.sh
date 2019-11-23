601e5a150f44a6ce17855c8576068a18154b289f
Ryan L McIntyre <ryanoasis@users.noreply.github.com>
2019-11-22 20:58:44 -0800

    Fixes version bumping

--- bin/scripts/version-bump.sh
+++ bin/scripts/version-bump.sh
@@ -17,7 +17,10 @@ fi
 release=$1
 
 sed -i "s|[0-9]\\.[0-9]\\.[0-9]|$release|g" ../../font-patcher
-sed -i "s|\\# nerd fonts version: [0-9]\\.[0-9]\\.[0-9]|\\# version: $release|g" ../../bin/scripts/*.sh
+sed -i "s|\\# Nerd Fonts Version: [0-9]\\.[0-9]\\.[0-9]|\\# Nerd Fonts Version: $release|g" ../../bin/scripts/**/*.sh
+sed -i "s|\\# Nerd Fonts Version: [0-9]\\.[0-9]\\.[0-9]|\\# Nerd Fonts Version: $release|g" ../../bin/scripts/**/*.py
+sed -i "s|\\# Nerd Fonts Version: [0-9]\\.[0-9]\\.[0-9]|\\# Nerd Fonts Version: $release|g" ../../bin/scripts/*.sh
+sed -i "s|\\# Nerd Fonts Version: [0-9]\\.[0-9]\\.[0-9]|\\# Nerd Fonts Version: $release|g" ../../bin/scripts/*.py
 sed -i "s|\\# version: [0-9]\\.[0-9]\\.[0-9]|\\# version: $release|g" ../../bin/scripts/*.sh
 sed -i "s|version=\"[0-9]\\.[0-9]\\.[0-9]\"|version=\"$release\"|g" ../../bin/scripts/*.sh
 
