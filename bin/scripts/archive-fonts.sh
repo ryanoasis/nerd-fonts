0b66b58fa8bc66d6745fe90e0d6d162d6897a039
Ryan L McIntyre <ryanoasis@users.noreply.github.com>
2020-01-18 21:18:14 -0800

    Use the most optimal compression level of zip just in case

--- bin/scripts/archive-fonts.sh
+++ bin/scripts/archive-fonts.sh
@@ -45,12 +45,12 @@ do
   [[ -d "$outputdir" ]] || mkdir -p "$outputdir"
 
   # -ic (ignore case not working)
-  zip "$outputdir/$basename" -rj "$searchdir" -i '*.[o,t]tf' -i '*.[O,T]TF'
+  zip -9 "$outputdir/$basename" -rj "$searchdir" -i '*.[o,t]tf' -i '*.[O,T]TF'
   zipStatus=$?
   if [ "$zipStatus" != "0" ]
   then
     echo "$LINE_PREFIX Could not create archive with the path junked (-j option) - likely same font names for different paths, zip status: $zipStatus"
     echo "$LINE_PREFIX Retrying with full path"
-    zip "$outputdir/$basename" -r "$searchdir" -i '*.[o,t]tf' -i '*.[O,T]TF'
+    zip -9 "$outputdir/$basename" -r "$searchdir" -i '*.[o,t]tf' -i '*.[O,T]TF'
   fi;
 done
