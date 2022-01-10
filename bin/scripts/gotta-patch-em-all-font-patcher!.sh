1877d0289018240752bfd59e4a4b348827faa74d
Fini Jastrow <ulf.fini.jastrow@desy.de>
2022-01-10 08:35:38 +0100

    patch-em-all: Allow to specify font specific options
    [why]
    We want to patch some fonts with different font-patcher options.
    
    [how]
    Use the config.cfg file that each source font can have to specify one
    arbitrary option to the font-patcher calls.
    
    [note]
    This is partially commit 9e2bc9a26 from #723.
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- bin/scripts/gotta-patch-em-all-font-patcher!.sh
+++ bin/scripts/gotta-patch-em-all-font-patcher!.sh
@@ -101,6 +101,7 @@ function patch_font {
   config_dir=$( cd "$( dirname "$f" )" && pwd)
 
   # source the font config file if exists:
+  # fetches for example config_patch_flags
   if [ -f "$config_dir/config.cfg" ]
   then
     # shellcheck source=/dev/null
@@ -143,10 +144,10 @@ function patch_font {
   }
   # Use absolute path to allow fontforge being an AppImage (used in CI)
   PWD=`pwd`
-  fontforge -quiet -script $PWD/font-patcher "$f" -q $powerline $post_process --complete --no-progressbars --outputdir "${patched_font_dir}complete/" 2>/dev/null
-  fontforge -quiet -script $PWD/font-patcher "$f" -q -s ${font_config} $powerline $post_process --complete --no-progressbars --outputdir "${patched_font_dir}complete/" 2>/dev/null
-  fontforge -quiet -script $PWD/font-patcher "$f" -q -w $powerline $post_process --complete --no-progressbars --outputdir "${patched_font_dir}complete/" 2>/dev/null
-  fontforge -quiet -script $PWD/font-patcher "$f" -q -s ${font_config} -w $powerline $post_process --complete --no-progressbars --outputdir "${patched_font_dir}complete/" 2>/dev/null
+  fontforge -quiet -script $PWD/font-patcher "$f" -q $powerline $post_process --complete --no-progressbars --outputdir "${patched_font_dir}complete/" $config_patch_flags 2>/dev/null
+  fontforge -quiet -script $PWD/font-patcher "$f" -q -s ${font_config} $powerline $post_process --complete --no-progressbars --outputdir "${patched_font_dir}complete/" $config_patch_flags 2>/dev/null
+  fontforge -quiet -script $PWD/font-patcher "$f" -q -w $powerline $post_process --complete --no-progressbars --outputdir "${patched_font_dir}complete/" $config_patch_flags 2>/dev/null
+  fontforge -quiet -script $PWD/font-patcher "$f" -q -s ${font_config} -w $powerline $post_process --complete --no-progressbars --outputdir "${patched_font_dir}complete/" $config_patch_flags 2>/dev/null
   # wait for this group of background processes to finish to avoid forking too many processes
   # that can add up quickly with the number of combinations
   #wait
