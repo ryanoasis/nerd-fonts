b72c826d2f54a7669d4fd4e63830646fce671ee8
Fini Jastrow <ulf.fini.jastrow@desy.de>
2022-11-25 12:52:23 +0100

    casks: Clean up whitespace
    [why]
    Several different indentations methods used here. That confuses me and I
    want to edit this file.
    
    [how]
    Unify to '4 blanks' as used in other (recently edited) files.
    Unify if-then and while-do syntax.
    
    Also replace single quotes with double quite in the cask file.
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- bin/scripts/generate-casks.sh
+++ bin/scripts/generate-casks.sh
@@ -17,121 +17,115 @@ appcastcheckpoint=$(curl --compressed --location --user-agent 'Mozilla/5.0 (Maci
 LINE_PREFIX="# [Nerd Fonts] "
 
 cd $patched_parent_dir || {
-  echo >&2 "$LINE_PREFIX Could not find patched fonts directory"
-  exit 1
+    echo >&2 "$LINE_PREFIX Could not find patched fonts directory"
+    exit 1
 }
 
 function clear_file {
-  local outputfile=$1
-	# clear output file (needed for multiple runs or updates):
-	true > "$outputfile" 2> /dev/null
+    local outputfile=$1
+    # clear output file (needed for multiple runs or updates):
+    true > "$outputfile" 2> /dev/null
 }
 
 function write_header {
-  local outputfile=$1
-  local caskname=$2
+    local outputfile=$1
+    local caskname=$2
 
-  {
-    printf "cask '%s' do\\n" "$caskname"
-    printf "  version '%s'\\n" "$version"
-    printf "  sha256 '%s'\\n\\n" "$sha256sum"
-    printf "  url \"%s%s.zip\"\\n" "$downloadarchive" "$basename"
-    printf "  appcast '%s',\\n" "$appcast"
-    printf "          checkpoint: '%s'\\n" "$appcastcheckpoint"
-  } >> "$outputfile"
+    {
+        printf "cask \"%s\" do\\n" "$caskname"
+        printf "  version \"%s\"\\n" "$version"
+        printf "  sha256 \"%s\"\\n\\n" "$sha256sum"
+        printf "  url \"%s%s.zip\"\\n" "$downloadarchive" "$basename"
+        printf "  appcast '%s',\\n" "$appcast"
+        printf "          checkpoint: '%s'\\n" "$appcastcheckpoint"
+    } >> "$outputfile"
 }
 
 function write_body {
-	local outputfile=$1
-	shift;
-	local fonts=("$@")
+    local outputfile=$1
+    shift;
+    local fonts=("$@")
 
-	if [ "${fonts[0]}" ];
-	then
-		for i in "${!fonts[@]}"
-		do
-			individualfont=$(basename "${fonts[$i]}")
+    if [ "${fonts[0]}" ]; then
+        for i in "${!fonts[@]}"; do
+            individualfont=$(basename "${fonts[$i]}")
 
-			printf "## Found Font: %s\\n" "${fonts[$i]}"
+            printf "## Found Font: %s\\n" "${fonts[$i]}"
 
-			if [ "$i" == 0 ];
-			then
-				familyname=$(fc-query --format='%{family}' "${fonts[$i]}")
-				{
-					printf "  name '%s (%s)'\\n" "$familyname" "$basename"
-					printf "  homepage '%s'" "$homepage"
-					printf "\\n\\n"
-				} >> "$outputfile"
-			fi
+            if [ "$i" == 0 ]; then
+                familyname=$(fc-query --format='%{family}' "${fonts[$i]}")
+                {
+                    printf "  name \"%s (%s)\"\\n" "$familyname" "$basename"
+                    printf "  homepage \"%s\"" "$homepage"
+                    printf "\\n\\n"
+                } >> "$outputfile"
+            fi
 
-			printf "  font '%s'\\n" "$individualfont" >> "$outputfile"
+            printf "  font \"%s\"\\n" "$individualfont" >> "$outputfile"
 
-		done
-	else
-		echo "$LINE_PREFIX Did not find TTF or OTF"
-	fi
+        done
+    else
+        echo "$LINE_PREFIX Did not find TTF or OTF"
+    fi
 }
 
 function write_footer {
-  local outputfile=$1
+    local outputfile=$1
 
-  {
-    printf "end\\n"
-  } >> "$outputfile"
+    {
+        printf "end\\n"
+    } >> "$outputfile"
 }
 
 pattern=$1
 if [ "$pattern" = "" ]; then
-	pattern=".*"
+    pattern=".*"
 fi
 
 #find ./Hack -maxdepth 0 -type d | # uncomment to test 1 font
 #find ./ProFont -maxdepth 2 -type d | # uncomment to test 1 font
 find . -maxdepth 1 -mindepth 1 -type d -iregex "\./$pattern" |
-while read -r filename
-do
+while read -r filename; do
 
-	dirname=$(dirname "$filename")
-	basename=$(basename "$filename")
-	sha256sum=$(sha256sum "../archives/${basename}.zip" | head -c 64)
-	searchdir=$filename
+    dirname=$(dirname "$filename")
+    basename=$(basename "$filename")
+    sha256sum=$(sha256sum "../archives/${basename}.zip" | head -c 64)
+    searchdir=$filename
 
-	MONOFONTS=()
-	while IFS= read -d $'\0' -r file ; do
-	  MONOFONTS=("${MONOFONTS[@]}" "$file")
-	done < <(find "$searchdir" -type f -iwholename '*complete*' \( -iname '*.[o,t]tf' ! -wholename '*Windows*' -iname '*complete mono*' \) -print0)
+    MONOFONTS=()
+    while IFS= read -d $'\0' -r file; do
+        MONOFONTS=("${MONOFONTS[@]}" "$file")
+    done < <(find "$searchdir" -type f -iwholename '*complete*' \( -iname '*.[o,t]tf' ! -wholename '*Windows*' -iname '*complete mono*' \) -print0)
 
-	FONTS=()
-	while IFS= read -d $'\0' -r file ; do
-	  FONTS=("${FONTS[@]}" "$file")
-	done < <(find "$searchdir" -type f -iwholename '*complete*' \( -iname '*.[o,t]tf' ! -wholename '*Windows*' ! -iname '*complete mono*' \) -print0)
+    FONTS=()
+    while IFS= read -d $'\0' -r file; do
+        FONTS=("${FONTS[@]}" "$file")
+    done < <(find "$searchdir" -type f -iwholename '*complete*' \( -iname '*.[o,t]tf' ! -wholename '*Windows*' ! -iname '*complete mono*' \) -print0)
 
-	outputdir=$PWD/../casks
+    outputdir=$PWD/../casks
 
-	formattedbasename=$(echo "$basename" | tr "[:upper:]" "[:lower:]")
+    formattedbasename=$(echo "$basename" | tr "[:upper:]" "[:lower:]")
 
-	echo "$LINE_PREFIX Generating cask for: $basename"
+    echo "$LINE_PREFIX Generating cask for: $basename"
 
-	[[ -d "$outputdir" ]] || mkdir -p "$outputdir"
+    [[ -d "$outputdir" ]] || mkdir -p "$outputdir"
 
-	caskname="font-$formattedbasename-nerd-font"
-	caskname_mono="${caskname}-mono"
-	to="$outputdir/${caskname}.rb"
-	to_mono="$outputdir/${caskname_mono}.rb"
+    caskname="font-$formattedbasename-nerd-font"
+    caskname_mono="${caskname}-mono"
+    to="$outputdir/${caskname}.rb"
+    to_mono="$outputdir/${caskname_mono}.rb"
 
-  clear_file "$to"
-  write_header "$to" "$caskname"
+    clear_file "$to"
+    write_header "$to" "$caskname"
 
-  clear_file "$to_mono"
-  write_header "$to_mono" "$caskname_mono"
+    clear_file "$to_mono"
+    write_header "$to_mono" "$caskname_mono"
 
-  write_body "$to" "${FONTS[@]}"
-  write_body "$to_mono" "${MONOFONTS[@]}"
+    write_body "$to" "${FONTS[@]}"
+    write_body "$to_mono" "${MONOFONTS[@]}"
 
-  write_footer "$to"
-  write_footer "$to_mono"
+    write_footer "$to"
+    write_footer "$to_mono"
 
-  echo "## Created casks: ${outputdir}/${caskname}*.rb"
-
+    echo "## Created casks: ${outputdir}/${caskname}*.rb"
 done
-
