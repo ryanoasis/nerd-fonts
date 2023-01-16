e6c01bc5fbcf54b2f430a6c86459381091a2e5a5
Fini Jastrow <ulf.fini.jastrow@desy.de>
2023-01-16 23:48:35 +0100

    Fix Cheat Sheet for new MDI [skip ci]
    [why]
    Although commit
      69e8c0e  Add current Material Design Icons
    
    claims that we updated the Cheat Sheet after adding the new Material
    Design Icons, that actually did not happen.
    
    [how]
    Add new MDI's i_*.sh file to all. That is used by the generate-css that
    also generates the cheat sheet web page.
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- bin/scripts/lib/i_all.sh
+++ bin/scripts/lib/i_all.sh
@@ -2,7 +2,7 @@
 # Nerd Fonts Version: 2.3.0-RC
 # Script Version 1.1.0
 
-for i in $(dirname "${BASH_SOURCE[0]:-$0}")/i_{cod,dev,fae,fa,iec,logos,oct,ple,pom,seti,material,weather}.sh; do
+for i in $(dirname "${BASH_SOURCE[0]:-$0}")/i_{cod,dev,fae,fa,iec,logos,oct,ple,pom,seti,material,weather,md}.sh; do
 	# shellcheck source=/dev/null
 	test -f "$i" -a -r "$i" && source "$i"
 done
