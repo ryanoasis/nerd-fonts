8563b0cdff6ded7848ea19a127a599a6471ed942
Fini Jastrow <ulf.fini.jastrow@desy.de>
2023-11-10 20:51:55 +0100

    Monaspace: Fix updater script
    [why]
    Shellcheck complains that quotes are missing.
    We know that there will not be any problem ever as the elements are hard
    coded, but fix to be warning free.
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- src/unpatched-fonts/Monaspace/update.sh
+++ src/unpatched-fonts/Monaspace/update.sh
@@ -5,19 +5,19 @@ TMP_DIR=$(mktemp -d)
 
 set -e
 
-curl -LOf "https://github.com/githubnext/monaspace/releases/download/v${FONT_VERSION}/monaspace-v${FONT_VERSION}.zip" --output-dir "$TMP_DIR"
+curl -LOf "https://github.com/githubnext/monaspace/releases/download/v${FONT_VERSION}/monaspace-v${FONT_VERSION}.zip" --output-dir "${TMP_DIR}"
 unzip -d "${TMP_DIR}" "${TMP_DIR}/*.zip"
-ls -alh $TMP_DIR
+ls -alh "${TMP_DIR}"
 
 # In order of increasing atomic mass:
 typefaces=(Neon Argon Krypton Xenon Radon)
 
 weights=(Regular Italic Bold BoldItalic Medium MediumItalic)
 
-for face in ${typefaces[@]}; do
+for face in "${typefaces[@]}"; do
     echo "Processing ${face} ..."
     mkdir -p "${face}"
-    for weight in ${weights[@]}; do
+    for weight in "${weights[@]}"; do
         find "${TMP_DIR}" -name "Monaspace${face}-${weight}.otf" -exec mv "{}" "${face}" \;
     done
 done
