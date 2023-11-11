693ac495b384ba7c0f43faec3c3f5f3b38536ad8
Fini Jastrow <ulf.fini.jastrow@desy.de>
2023-11-11 19:11:34 +0100

    Martian: Drop many weights and adapt paths
    SemiWide and Narrow might be nice in some contexts, but that is not
    really the audience of Nerd Fonts I guess.
    
    Lets start with few and we can always add more if there is really a
    demand.
    
    Simplify the paths; the old style stems from the time when we patched
    all variants per source font. Nowerdays the fonts are few (3 per source)
    and will almost always be installed together anyhow.
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- src/unpatched-fonts/MartianMono/update.sh
+++ src/unpatched-fonts/MartianMono/update.sh
@@ -7,43 +7,22 @@
 FONT_VERSION=1.0.0
 TMP_DIR=$(mktemp -d)
 
-set -ex
+set -e
 
-curl -L "https://github.com/evilmartians/mono/releases/download/v${FONT_VERSION}/martian-mono-${FONT_VERSION}-ttf.zip" | tar -xf - -C $TMP_DIR
-ls -alh $TMP_DIR
+curl -LOf "https://github.com/evilmartians/mono/releases/download/v${FONT_VERSION}/martian-mono-${FONT_VERSION}-ttf.zip" --output-dir "${TMP_DIR}"
+unzip -d "${TMP_DIR}" "${TMP_DIR}/*.zip"
+ls -alh "${TMP_DIR}"
 
-mv "${TMP_DIR}/MartianMono-CnBd.ttf" ./Condensed-Bold/
-mv "${TMP_DIR}/MartianMono-CnLt.ttf" ./Condensed-Light/
-mv "${TMP_DIR}/MartianMono-CnMd.ttf" ./Condensed-Medium/
-mv "${TMP_DIR}/MartianMono-CnRg.ttf" ./Condensed-Regular/
-#mv "${TMP_DIR}/MartianMono-CnSb.ttf" ./Condensed-SemiBold/
-mv "${TMP_DIR}/MartianMono-CnTh.ttf" ./Condensed-Thin/
-mv "${TMP_DIR}/MartianMono-CnxBd.ttf" ./Condensed-ExtraBold/
-mv "${TMP_DIR}/MartianMono-CnxLt.ttf" ./Condensed-ExtraLight/
+# typefaces=(Cn Nr Std sWd) # Condensed Narrow Standard SemiWide
+typefaces=(Cn Std)
 
-mv "${TMP_DIR}/MartianMono-NrBd.ttf" ./Narrow-Bold/
-mv "${TMP_DIR}/MartianMono-NrLt.ttf" ./Narrow-Light/
-mv "${TMP_DIR}/MartianMono-NrMd.ttf" ./Narrow-Medium/
-mv "${TMP_DIR}/MartianMono-NrRg.ttf" ./Narrow-Regular/
-#mv "${TMP_DIR}/MartianMono-NrSb.ttf" ./Narrow-SemiBold/
-mv "${TMP_DIR}/MartianMono-NrTh.ttf" ./Narrow-Thin/
-mv "${TMP_DIR}/MartianMono-NrxBd.ttf" ./Narrow-ExtraBold/
-mv "${TMP_DIR}/MartianMono-NrxLt.ttf" ./Narrow-ExtraLight/
+# weights=(Th xLt Lt Rg Md Bd xBd) # Thin ExtraLight Light Regular Medium Bold ExtraBold
+weights=(Rg Md Bd)
 
-mv "${TMP_DIR}/MartianMono-StdBd.ttf" ./Standard-Bold/
-mv "${TMP_DIR}/MartianMono-StdLt.ttf" ./Standard-Light/
-mv "${TMP_DIR}/MartianMono-StdMd.ttf" ./Standard-Medium/
-mv "${TMP_DIR}/MartianMono-StdRg.ttf" ./Standard-Regular/
-#mv "${TMP_DIR}/MartianMono-StdSb.ttf" ./Standard-SemiBold/
-mv "${TMP_DIR}/MartianMono-StdTh.ttf" ./Standard-Thin/
-mv "${TMP_DIR}/MartianMono-StdxBd.ttf" ./Standard-ExtraBold/
-mv "${TMP_DIR}/MartianMono-StdxLt.ttf" ./Standard-ExtraLight/
-
-mv "${TMP_DIR}/MartianMono-sWdBd.ttf" ./SemiWide-Bold/
-mv "${TMP_DIR}/MartianMono-sWdLt.ttf" ./SemiWide-Light/
-mv "${TMP_DIR}/MartianMono-sWdMd.ttf" ./SemiWide-Medium/
-mv "${TMP_DIR}/MartianMono-sWdRg.ttf" ./SemiWide-Regular/
-#mv "${TMP_DIR}/MartianMono-sWdSb.ttf" ./SemiWide-SemiBold/
-mv "${TMP_DIR}/MartianMono-sWdTh.ttf" ./SemiWide-Thin/
-mv "${TMP_DIR}/MartianMono-sWdxBd.ttf" ./SemiWide-ExtraBold/
-mv "${TMP_DIR}/MartianMono-sWdxLt.ttf" ./SemiWide-ExtraLight/
+for face in "${typefaces[@]}"; do
+    echo "Processing ${face} ..."
+    mkdir -p "${face}"
+    for weight in "${weights[@]}"; do
+        find "${TMP_DIR}" -name "MartianMono-${face}${weight}.ttf" -exec mv "{}" "${face}" \;
+    done
+done
