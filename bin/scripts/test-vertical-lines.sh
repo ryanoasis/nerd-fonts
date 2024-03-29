758307300b57fef9a6b86cfb0df070a77b43faf0
Fini Jastrow <ulf.fini.jastrow@desy.de>
2024-03-29 13:35:57 +0100

    tests: Fix shellcheck issue
    SC2059 (info): Don't use variables in the printf format string. Use printf '..%s..' "$foo".
    
    Which is hard(er) because the sequences are not interpreted unless they
    are in the format string.
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- bin/scripts/test-vertical-lines.sh
+++ bin/scripts/test-vertical-lines.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 # Nerd Fonts Version: 3.1.1
-# Script Version: 1.0.0
+# Script Version: 1.0.1
 #
 # A more conceise version of the Powerline test, designed to show
 # the dreaded 'faint vertical lines' phenomenon from LCD antialiasing
 
-COL_A='\033[48;5;51m\033[38;5;200m'
-COL_B='\033[48;5;200m\033[38;5;51m'
+COL_A=$(printf '\033[48;5;51m\033[38;5;200m')
+COL_B=$(printf '\033[48;5;200m\033[38;5;51m')
+COL_C=$(printf '\033[48;5;238m\033[38;5;253m')
+COL_D=$(printf '\033[48;5;253m\033[38;5;238m')
 
 printf "\
-${COL_A}                                                         \n\
-Under \ue0b2${COL_B} lap ${COL_A}\ue0b0 and \ue0b6${COL_B} top ${COL_A}\ue0b4 mop             \n\
-${COL_A}Over \ue0b2${COL_B} lap ${COL_A}\ue0b0 and \ue0b6${COL_B} top ${COL_A}\ue0b4 mop trop lop klop\n\
-                                      \n"
-
-COL_A='\033[48;5;238m\033[38;5;253m'
-COL_B='\033[48;5;253m\033[38;5;238m'
+%s                                                         \n\
+Under \ue0b2%s lap %s\ue0b0 and \ue0b6%s top %s\ue0b4 mop             \n\
+%sOver \ue0b2%s lap %s\ue0b0 and \ue0b6%s top %s\ue0b4 mop trop lop klop\n\
+                                      \n" \
+"${COL_A}" "${COL_B}" "${COL_A}" "${COL_B}" "${COL_A}" \
+"${COL_A}" "${COL_B}" "${COL_A}" "${COL_B}" "${COL_A}"
 
 printf "\
-${COL_A}                                                         \n\
-Under \ue0b2${COL_B} lap ${COL_A}\ue0b0 and \ue0b6${COL_B} top ${COL_A}\ue0b4 mop             \n\
-${COL_A}Over \ue0b2${COL_B} lap ${COL_A}\ue0b0 and \ue0b6${COL_B} top ${COL_A}\ue0b4 mop trop lop klop\n\
-                                      \n"
+%s                                                         \n\
+Under \ue0b2%s lap %s\ue0b0 and \ue0b6%s top %s\ue0b4 mop             \n\
+%sOver \ue0b2%s lap %s\ue0b0 and \ue0b6%s top %s\ue0b4 mop trop lop klop\n\
+                                      \n" \
+"${COL_C}" "${COL_D}" "${COL_C}" "${COL_D}" "${COL_C}" \
+"${COL_C}" "${COL_D}" "${COL_C}" "${COL_D}" "${COL_C}"
