bdbf49868ced6225a9dbd069e7461dc1a9956dc8
Marcin Różewski <marcinrozewski@gmail.com>
2024-06-23 13:10:28 +0200

    style: Sort IEC Power Symbols shell script

--- bin/scripts/lib/i_iec.sh
+++ bin/scripts/lib/i_iec.sh
@@ -3,8 +3,8 @@
 # Codepoints: 23FB-23FE 2B58
 test -n "$__i_iec_loaded" && return || __i_iec_loaded=1
 i='⏻' i_iec_power=$i
-i='⏼' i_iec_toggle_power=$i
+i='⭘' i_iec_power_off=$i
 i='⏽' i_iec_power_on=$i
 i='⏾' i_iec_sleep_mode=$i
-i='⭘' i_iec_power_off=$i
+i='⏼' i_iec_toggle_power=$i
 unset i
