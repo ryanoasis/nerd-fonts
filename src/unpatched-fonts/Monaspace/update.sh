9c7fa1c9a7fdf96d07d2a89cb57815fc1f17272c
Fini Jastrow <ulf.fini.jastrow@desy.de>
2024-06-30 17:50:54 +0200

    Monaspace: Add ExtraLight Italic
    Fixup of previous commit.
    Fixes: #1670
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- src/unpatched-fonts/Monaspace/update.sh
+++ src/unpatched-fonts/Monaspace/update.sh
@@ -12,7 +12,7 @@ ls -alh "${TMP_DIR}"
 # In order of increasing atomic mass:
 typefaces=(Neon Argon Krypton Xenon Radon)
 
-weights=(ExtraLight Light LightItalic Regular Italic Bold BoldItalic Medium MediumItalic)
+weights=(ExtraLight ExtraLightItalic Light LightItalic Regular Italic Bold BoldItalic Medium MediumItalic)
 
 for face in "${typefaces[@]}"; do
     echo "Processing ${face} ..."
