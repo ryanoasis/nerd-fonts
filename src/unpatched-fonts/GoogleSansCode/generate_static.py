4abf302389b93e4373b8f6b6e7ff075677347ecc
Fini Jastrow <ulf.fini.jastrow@desy.de>
2026-07-31 01:25:24 +0200

    Improve Google Sans Code generator
    Mostly comments ;)
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- src/unpatched-fonts/GoogleSansCode/generate_static.py
+++ src/unpatched-fonts/GoogleSansCode/generate_static.py
@@ -17,21 +17,22 @@ fonts = [
 weights = {
     #'Thin': 100,
     #'ExtraLight': 200,
-     'Light': 300,
-     'Regular': 400,
-     'Medium': 500,
-     'SemiBold': 600,
-     'Bold': 700,
+    'Light': 300,
+    'Regular': 400,
+    'Medium': 500,
+    'SemiBold': 600,
+    'Bold': 700,
     'ExtraBold': 800,
     #'Black': 900,
 }
 
 def update_font_names(font, weight_name):
     """Analyse the name(s) and put the weight in the appropriate position"""
-
+    # This code is not 100% generic and might or might not work for other
+    # fonts than Google Sans Code
     fullname_parts = [ '', '' ]
     ribbi_weight = weight_name in [ 'Regular', 'Bold' ]
-    for name in font['name'].names:
+    for name in sorted(font['name'].names, key=lambda e: e.nameID):
         if name.nameID not in (
                 NameID.FAMILY_NAME,
                 NameID.SUBFAMILY_NAME,
@@ -42,14 +43,20 @@ def update_font_names(font, weight_name):
                 NameID.TYPOGRAPHIC_SUBFAMILY_NAME):
             continue
 
-        # ID 1   Google Sans Code                    Google Sans Code
-        # ID 2   Regular                             Italic
-        # ID 3   7.001;GOOG;GoogleSansCode-Regular   7.001;GOOG;GoogleSansCode-Italic
-        # ID 4   Google Sans Code Regular            Google Sans Code Italic
-        # ID 6   GoogleSansCode-Regular              GoogleSansCode-Italic
+        # Names stored in the VF:
+        #
+        # ID  1   Google Sans Code                    Google Sans Code
+        # ID  2   Regular                             Italic
+        # ID  3   7.001;GOOG;GoogleSansCode-Regular   7.001;GOOG;GoogleSansCode-Italic
+        # ID  4   Google Sans Code Regular            Google Sans Code Italic
+        # ID  6   GoogleSansCode-Regular              GoogleSansCode-Italic
+        # ID 16   -                                   -
+        # ID 17   -                                   -
 
         current_name = name.toUnicode()
 
+        # Add the new weight to the FAMILY if it is not a ribbi weight
+        # Add it to the SUBFAMILY if it is a ribbi weight
 
         if name.nameID == NameID.FAMILY_NAME:
             if not ribbi_weight:
@@ -65,8 +72,12 @@ def update_font_names(font, weight_name):
             if current_name != 'Regular' or ribbi_weight:
                 fullname_parts[1] = current_name
 
+        # Assemble the FULLNAME from the basic parts
+        # Repurpose the basic parts for the TYPO FAMILY by shifting
+        # part of the style to the front
+
         elif name.nameID == NameID.FULL_FONT_NAME:
-            # The names come in ID order so we are sure we collected the name here
+            # The names come in ID order so we are sure we collected the name before
             current_name = ' '.join(fullname_parts).strip()
             # Add typogr. family, maybe, after tweaking ID4
             if not ribbi_weight:
@@ -86,6 +97,8 @@ def update_font_names(font, weight_name):
                     langID=name.langID,
                 )
 
+        # Standalone code for the blank-less and hyphen containing names
+
         elif name.nameID in [ NameID.POSTSCRIPT_NAME, NameID.UNIQUE_FONT_IDENTIFIER ]:
             current_name = current_name.replace('Regular', '')
             parts = current_name.split('-')
@@ -95,7 +108,12 @@ def update_font_names(font, weight_name):
             if name.nameID == NameID.UNIQUE_FONT_IDENTIFIER:
                 current_name += ';NF'
 
-        name.string = current_name.encode(name.getEncoding())
+        font['name'].setName(current_name,
+            nameID=name.nameID,
+            platformID=name.platformID,
+            platEncID=name.platEncID,
+            langID=name.langID,
+        )
 
 for f in fonts:
     font = ttLib.TTFont(f)
