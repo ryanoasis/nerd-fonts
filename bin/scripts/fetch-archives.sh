be2792380de149388db88e1b999923ff3c5910b0
Fini Jastrow <ulf.fini.jastrow@desy.de>
2026-08-02 20:51:11 +0200

    fetch-archives: Allow to download draft-release artifacts
    [why]
    I always struggle to get the artifacts downloaded when releasing,
    because I usually need them before the release has been published. Then
    the links are different and some authorization is needed.
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- bin/scripts/fetch-archives.sh
+++ bin/scripts/fetch-archives.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Nerd Fonts Version: 3.5.0
-# Script Version: 2.0.1
+# Script Version: 2.1.0
 #
 # Fetches the current release files.
 # It fetches the latest release, not release candidate.
@@ -11,17 +11,27 @@
 # be fetched.
 #
 # Example runs
-#   fetch-archives
-#   fetch-archives v2.2.2
-#   fetch-archives v2.2.2 Heavy
-#   fetch-archives latest HeavyDat
-#   fetch-archives v3.1.0 'Ara.*zip'   (just fetch the zip archive)
+#   fetch-archives.sh
+#   fetch-archives.sh v2.2.2
+#   fetch-archives.sh v2.2.2 Heavy
+#   fetch-archives.sh latest HeavyDat
+#   fetch-archives.sh v3.1.0 'Ara.*zip'   (just fetch the zip archive)
+#   GITHUB_TOKEN=1234567890 fetch-archives.sh v5.4.3   (draft release)
 
 set -e
 
 LINE_PREFIX="# [Nerd Fonts] "
 scripts_root_dir="$(cd "$(dirname "$0")" && pwd)"
 outputdir=$(realpath "${scripts_root_dir}/../../archives")
+
+# For draft releases a read-repository-token is needed to access the assets
+# Set it in the GITHUB_TOKEN variable when calling
+if [ -z "${GITHUB_TOKEN}" ]; then
+    echo "${LINE_PREFIX} No GITHUB_TOKEN set, limiting to public releases"
+else
+    echo "${LINE_PREFIX} Using GITHUB_TOKEN ${GITHUB_TOKEN}"
+    AUTH="Authorization: Bearer ${GITHUB_TOKEN}"
+fi
 
 if [ $# -ge 1 ]; then
     versiontag=$1
@@ -30,29 +40,29 @@ else
 fi
 
 if [ "${versiontag}" != "latest" ]; then
-    echo "${LINE_PREFIX} Fetching release archives with version tag '${versiontag}'"
-    releasedata=$(curl -Lf "https://api.github.com/repos/ryanoasis/nerd-fonts/releases")
-    num=$(jq ".[] | select(.tag_name == \"${versiontag}\") | .assets | length" <<< "${releasedata}")
-    if [ -z "${num}" ]; then
+    releasedata=${outputdir}/releases.json
+    if [ -f "${releasedata}" ]; then
+        echo "${LINE_PREFIX} Reusing existing releases.json file. Remove file to force update."
+    else
+        echo "${LINE_PREFIX} Fetching all releases metadata"
+        curl -H "${AUTH}" -Lf "https://api.github.com/repos/ryanoasis/nerd-fonts/releases" -o "${releasedata}"
+    fi
+    echo "${LINE_PREFIX} Fetching release metadata with version tag '${versiontag}'"
+    if [ -z "$(jq ".[] | select(.tag_name == \"${versiontag}\")" "${releasedata}")" ]; then
         echo "${LINE_PREFIX} Release tag ${versiontag} unknown"
         exit 1
     fi
-    files=()
-    while IFS='' read -r file; do
-        files+=("$file")
-    done < <(jq -r ".[] | select(.tag_name == \"${versiontag}\") | .assets[].name | @sh" <<< "${releasedata}")
+    # shellcheck disable=SC2207 # We know the output is well-behaved
+    assets=( $(jq -r ".[] | select(.tag_name == \"${versiontag}\") | .assets[] | \"\(.name)|\(.url)\" " "${releasedata}") )
 else
-    echo "${LINE_PREFIX} Fetching latest release archives"
-    releasedata=$(curl -Lf "https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest")
+    echo "${LINE_PREFIX} Fetching latest release metadata"
+    releasedata=$(curl -H "${AUTH}" -Lf "https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest")
     versiontag=$(jq -r ".tag_name" <<< "${releasedata}")
-    num=$(jq ".assets | length" <<< "${releasedata}")
-    files=()
-    while IFS='' read -r file; do
-        files+=("$file")
-    done < <(jq -r ".assets[].name | @sh" <<< "${releasedata}")
+    # shellcheck disable=SC2207 # We know the output is well-behaved
+    assets=( $(jq -r ".[] | select(.tag_name == \"${versiontag}\") | .assets[] | \"\(.name)|\(.url)\" " <<< "${releasedata}") )
 fi
 
-echo "${LINE_PREFIX} Found ${num} artifacts"
+echo "${LINE_PREFIX} Found ${#assets[@]} artifacts"
 
 if [ $# -ge 2 ]; then
     pattern=${2// /\\ }
@@ -66,14 +76,19 @@ if [ $# -gt 2 ]; then
     exit 2
 fi
 
-for assetname in "${files[@]}"; do
-    assetname=${assetname:1:-1}
-    if [[ ! "${assetname}" =~ ${pattern} ]]; then
+for asset in "${assets[@]}"; do
+    name=${asset%%|*}   # up to first |
+    url=${asset#*|}     # from first | to end
+
+    if [[ ! "${name}" =~ ${pattern} ]]; then
         continue
     fi
-    echo >&2 "${LINE_PREFIX} Fetching ${versiontag}/${assetname}"
+    echo >&2 "${LINE_PREFIX} Fetching ${versiontag} / ${name}"
     mkdir -p "${outputdir}"
     touch "${outputdir}/_Release_${versiontag}"
-    curl --fail -Lo "${outputdir}/${assetname}" "https://github.com/ryanoasis/nerd-fonts/releases/download/${versiontag}/${assetname}" \
-        || echo "   => error fetching"
+    curl --fail \
+        -H "${AUTH}" \
+        -H "Accept: application/octet-stream" \
+        -L -o "${outputdir}/${name}" \
+        "${url}" || echo "   => error fetching"
 done
